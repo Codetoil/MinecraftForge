363587812e90a319762d6df346a8cf0f04fe75ab
AterAnimAvis <AterAnimAvis@gmail.com>
2021-06-19 14:44:23 +0100

    partial_stash
    Signed-off-by: AterAnimAvis <AterAnimAvis@gmail.com>

---
+++ buildSrc/src/main/groovy/net/minecraftforge/forge/tasks/DownloadLibrariesTask.groovy
@@ -0,0 +1,38 @@
+package net.minecraftforge.forge.tasks
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
+
+public class DownloadLibrariesTask extends DefaultTask {
+    @InputFile File input
+    @OutputDirectory File output = project.file("build/${name}/")
+    Set<File> libraries = new HashSet<>()
+
+    @TaskAction
+    def run() {
+        Util.init()
+
+        def json = input.json().libraries.each { lib ->
+            //TODO: Thread?
+            def artifacts = [lib.downloads.artifact] + lib.downloads.get('classifiers', [:]).values()
+            artifacts.each{ art ->
+                def target = new File(output, art.path)
+                libraries.add(target)
+                if (!target.exists() || !art.sha1.equals(target.sha1())) {
+                    project.logger.lifecycle("Downloading ${art.url}")
+                    if (!target.parentFile.exists()) {
+                        target.parentFile.mkdirs()
+                    }
+                    new URL(art.url).withInputStream { i ->
+                        target.withOutputStream { it << i }
+                    }
+                    if (!art.sha1.equals(target.sha1())) {
+                        throw new IllegalStateException("Failed to download ${art.url} to ${target.canonicalPath} SHA Mismatch")
+                    }
+                }
+            }
+        }
+    }
+}
